c2ceb14774d7aa744e2aa83b039241edc2b0197f
romulo-rp29 <romul.ceuma@gmail.com>
2022-03-07 17:06:36 -0300

    update req 1

---
+++ src/pages/Favorites.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Favourites extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-favourites">
+        <p>TrybeTunes Favourites</p>
+      </div>
+    );
+  }
+}
+
+export default Favourites;
---
+++ src/pages/ProfileEdit.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ProfileEdit extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-profile-edit">
+        <p>TrybeTunes ProfileEdit</p>
+      </div>
+    );
+  }
+}
+
+export default ProfileEdit;
