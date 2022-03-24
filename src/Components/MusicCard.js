b2578d13338f3305ca60c013d2921a04ddfd064c
romulo-rp29 <romul.ceuma@gmail.com>
2022-03-24 18:18:56 -0300

    Revert "feat: MusicCard, Favorites"
    This reverts commit 6ca617f0dec42c905641d5d6a78fb1c8a8c7db16.

--- src/Components/MusicCard.js
+++ src/Components/MusicCard.js
@@ -3,10 +3,14 @@ import React, { Component } from 'react';
 export default class MusicCard extends Component {
   render() {
 <<<<<<< HEAD
+<<<<<<< HEAD
     const { music: { trackName, previewUrl, trackId } } = this.props;
 =======
     const { trackName, previewUrl } = this.state;
 >>>>>>> parent of 96a1c84... completed req7
+=======
+    const { music: { trackName, previewUrl } } = this.props;
+>>>>>>> parent of 73d8d4a... feat: MusicCard, Favorites
     return (
       <div>
         <h3>{ trackName }</h3>
@@ -18,6 +22,7 @@ export default class MusicCard extends Component {
           </code>
           .
         </audio>
+<<<<<<< HEAD
         <label htmlFor={ trackId }>
           <input
             type="checkbox"
@@ -28,6 +33,8 @@ export default class MusicCard extends Component {
           />
           Favorita
         </label>
+=======
+>>>>>>> parent of 73d8d4a... feat: MusicCard, Favorites
       </div>
     );
   }
@@ -38,7 +45,6 @@ MusicCard.propTypes = {
   music: PropTypes.shape({
     trackName: PropTypes.string.isRequired,
     previewUrl: PropTypes.string.isRequired,
-    trackId: PropTypes.string.isRequired,
   }),
 };
 MusicCard.defaultProps = {
--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import Header from '../Components/Header';
 import Loading from '../Components/Loading';
+<<<<<<< HEAD
 // import { addSong } from '../services/favoriteSongsAPI';
 
 export default class Favorites extends React.Component {
+=======
+
+export default class Favourites extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: false,
+    };
+  }
+
+>>>>>>> parent of 73d8d4a... feat: MusicCard, Favorites
   render() {
     const {
       loading,
